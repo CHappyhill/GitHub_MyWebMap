3ee179f9386a8c703dc56eab46500a5987c858b4
chappyhill <cornevreugdenhil@gmail.com>
2022-09-09 17:26:10 +0200

    Update...

---
+++ V0_SimpleWebpage/functions.js
@@ -0,0 +1,8 @@
+function RedButton_Run() {
+  // changeColor('red');
+  // document.body.digiclock.color = "black";
+  document.getElementById("digiclock").style.color = "yellow";
+  document.body.style.background = "red";
+  document.getElementById("RedButton").style.fontSize = "100px";
+  document.getElementById("RedButton").innerHTML = "Ah you did dare to press the button... Excellent!";
+}
